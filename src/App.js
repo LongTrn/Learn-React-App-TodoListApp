c3bfe214e18b494f1d3499c9c4a1d1511cf6871e
Long <longjax1401@gmail.com>
2020-06-13 19:55:27 +0700

    Add: Add new TodoItem method

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+      newItem: '',
       todoItems: [
         {title:'Đi chơi', isComplete: true},
         {title:'Đi học', isComplete: true},
@@ -15,6 +16,9 @@ class App extends Component {
         {title:'Đi ăn', isComplete: false}
       ]
     }
+
+    this.onKeyUp = this.onKeyUp.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
 
   onClickedItem(item){
@@ -37,13 +41,47 @@ class App extends Component {
     }; 
   }
 
+  onKeyUp(event) {
+    if (event.keyCode === 13) {// Enter as ASCII 
+      let text = event.target.value;
+      if(!text) {
+        return;
+      }
+  
+      text = text.trim();
+      if(!text) { return;}
+  
+      this.setState({
+        newItem: '',
+        todoItems: [
+          { title: text, isComplete:false },
+          ...this.state.todoItems
+        ]
+      });
+    }
+  }
+
+  onChange(event) {
+    this.setState({
+      newItem: event.target.value
+    });
+  }
+
   render() {
-    const { todoItems } = this.state;
+    const { todoItems, newItem } = this.state;
     if (todoItems.length) {
       return (
         <div className="App">
           <div className="Header">
-            <img src={checkAllComplete}/>
+            <img src={checkAllComplete} alt="CheckBox all items"/>
+            <input 
+              type="text" 
+              id="new-TodoItem" 
+              value={newItem}
+              onChange={this.onChange}
+              placeholder="Add new to-do item in list here."
+              onKeyUp={this.onKeyUp}
+            />
           </div>
           { todoItems.length > 0 && todoItems.map((item, index) => 
             <TodoItem 
