12840326d2ee369159ddd6e140ca487af5b1ca3b
Long <longjax1401@gmail.com>
2020-06-13 19:16:16 +0700

    Add: App Todo-List program

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import TodoItem from './components/TodoItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import checkAllComplete from './contents/images/checkbox/check-all-complete.svg';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todoItems: [
+        {title:'Đi chơi', isComplete: true},
+        {title:'Đi học', isComplete: true},
+        {title:'Đi ngủ', isComplete: true},
+        {title:'Đi ăn', isComplete: false}
+      ]
+    }
+  }
+
+  onClickedItem(item){
+    return(event) => {
+      console.log(item.title)
+      const isComplete = item.isComplete;
+      const { todoItems } = this.state;
+      const index = todoItems.indexOf(item);
+      this.setState({
+        todoItems: [
+          ...todoItems.slice(0, index), 
+          {
+            ...item,
+            isComplete: !isComplete
+          },
+          ...todoItems.slice(index + 1)
+        ]
+      })
+      
+    }; 
+  }
+
+  render() {
+    const { todoItems } = this.state;
+    if (todoItems.length) {
+      return (
+        <div className="App">
+          <div className="Header">
+            <img src={checkAllComplete}/>
+          </div>
+          { todoItems.length > 0 && todoItems.map((item, index) => 
+            <TodoItem 
+              key={index}  
+              item={item} 
+              onClick={this.onClickedItem(item)}
+            />
+          )}
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          { todoItems.length === 0 && 'Nothing in the list.'}
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
