ddb79b17d429efbeb7c35815abdf37c9d74a9c11
Long <longjax1401@gmail.com>
2020-06-13 19:13:13 +0700

    Add: TodoItem class

---
+++ src/components/TodoItem.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+
+import './TodoItem.css';
+import checkImg from '../contents/images/checkbox/check.svg'
+import checkCompleteImg from '../contents/images/checkbox/check-complete.svg'
+
+class TodoItem extends Component {
+    render() {
+        const { item, onClick } = this.props;
+        let className = 'TodoItem';
+        let url = checkImg;
+        if (item.isComplete) {
+            url = checkCompleteImg;
+        }
+        return (
+            <div  className={classNames(className, {
+                'TodoItem-Complete': item.isComplete
+            })}>
+                <img onClick={onClick} src={url}/>
+                <p>{item.title}</p>
+            </div>
+        )
+    }
+}
+
+export default TodoItem
