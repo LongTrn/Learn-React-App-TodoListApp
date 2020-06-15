f245bb80e6f77f85889450b2bf954fec814c428f
Long <longjax1401@gmail.com>
2020-06-15 16:35:14 +0700

    Add: current State of to-do List (all-active-completed)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import TodoItem from './components/TodoItem';
 
 import './App.css';
@@ -9,21 +10,26 @@ class App extends Component {
     super();
     this.state = {
       newItem: '',
+      currentState: 'all',
+      // currentState: 'active',
+      // currentState: 'completed',
+      //à ok :)))
       todoItems: [
         {title:'Đi chơi', isComplete: true},
         {title:'Đi học', isComplete: true},
         {title:'Đi ngủ', isComplete: true},
         {title:'Đi ăn', isComplete: false}
-      ]
+      ],
+      numberItems: 0
     }
 
     this.onKeyUp = this.onKeyUp.bind(this);
     this.onChange = this.onChange.bind(this);
+    this.changeState = this.changeState.bind(this);
   }
 
   onClickedItem(item){
     return(event) => {
-      console.log(item.title)
       const isComplete = item.isComplete;
       const { todoItems } = this.state;
       const index = todoItems.indexOf(item);
@@ -67,11 +73,55 @@ class App extends Component {
     });
   }
 
+  getContents(item, index) {
+    return (
+      <TodoItem 
+        key={index}  
+        item={item} 
+        onClick={this.onClickedItem(item)}
+      />
+    )
+  }
+
+  changeState(event) {
+    let state = event.target.value;
+    state = (state ==='All')? 'all' : (state === 'Active')? 'active' : 'completed'; 
+    this.setState({
+      currentState: state 
+    })
+  }
+
+  loadItembyState(todoItems, currentState) {
+    return todoItems.length > 0 && todoItems.map((item, index) => {
+      // filter khoi map luôn đúng không ông\
+    
+      // nhưng ma khỏ cái là nó return về value thôi chứ ko như map nó làm cái này luôn đc getContents
+      // ko dung filter ua dùng for đi :)))
+      //thấy map này nó cũng được mà 
+      //ủa tuỏng ông muốn bỏ dòng if cuối :vậy
+      // thi đúng tui định là chỉ so sánh thằng state nó là gì néu không nằm trong trường hợp là active hay completed thi luon return all ý 
+      // kiểu tui hiểu. nhưng mà thôi đừng làm vậy
+      // tui thử dung arrow này thử
+      if(currentState ==='active' && !item.isComplete){
+        return this.getContents(item, index)
+      } else if (currentState === 'completed' && item.isComplete) {
+        return this.getContents(item, index)
+      } else if (currentState === 'all') {
+        return this.getContents(item, index)
+      } else return '' //phai giu dong nay lai
+      //van giu như nay chu ko map no tra ve het,
+      // à tui hiểu để tui chỉnh sao để tránh trường hợp sai luôn cái 
+      
+    }) 
+  }
+
   render() {
-    const { todoItems, newItem } = this.state;
+    const { todoItems, newItem, currentState } = this.state;
+    let contents;
+    
     if (todoItems.length) {
       return (
-        <div className="App">
+        <div className="App" id="App">
           <div className="Header">
             <img src={checkAllComplete} alt="CheckBox all items"/>
             <input 
@@ -83,13 +133,37 @@ class App extends Component {
               onKeyUp={this.onKeyUp}
             />
           </div>
-          { todoItems.length > 0 && todoItems.map((item, index) => 
-            <TodoItem 
-              key={index}  
-              item={item} 
-              onClick={this.onClickedItem(item)}
-            />
-          )}
+          { 
+            this.loadItembyState(todoItems, currentState)
+          }
+          <div className="Footer">
+            <div className="currentItems">
+          <p>{todoItems.length} items left</p>
+            </div>
+            <div className="currentStates">
+              <input 
+                type="button" 
+                id="stateAll" 
+                name="stateAll" 
+                value="All"
+                onClick={this.changeState}
+                />
+              <input 
+                type="button" 
+                id="stateActive" 
+                name="stateActive" 
+                value="Active"
+                onClick={this.changeState}
+                />
+              <input 
+                type="button" 
+                id="stateCompleted" 
+                name="stateCompleted" 
+                value="Completed"
+                onClick={this.changeState}
+              />
+            </div>
+          </div>
         </div>
       );
     } else {
